94082a0c7a55d47294cd96ff616b8dac91970aa1
wzuqui <willianluiszuqui@gmail.com>
2022-12-28 04:10:27 -0300

    function component provou ser mais organizado no application

--- src/layouts/application.component.tsx
+++ src/layouts/application.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Component, PropsWithChildren } from 'react';
 
 import { MenuIcon } from '../assets/menu-icon';
@@ -9,120 +9,79 @@ import { IUser } from '../services/user.service.type';
 import { styled } from '../styles/theme';
 import { selectChildrenByType } from '../utils/selectChildrenByType';
 
-interface ApplicationProps extends PropsWithChildren<{}> {}
-interface ApplicationState {
-  usuario?: IUser;
-}
-
-export class Application extends Component<ApplicationProps, ApplicationState> {
-  public readonly service = ioc.get(ApplicationService);
-
-  constructor(props: ApplicationProps) {
-    super(props);
-    this.state = {};
-  }
-
-  async acaoLogin() {
-    await this.service.login();
-    this.setState((prev) => ({
-      ...prev,
-      usuario: this.service.usuario,
-    }));
-  }
-
-  render() {
-    if (!this.state.usuario) {
-      this.acaoLogin();
-      return <div>Em precesso de login!</div>;
-    }
-
-    const [logo, main] = selectChildrenByType(
-      this.props.children,
-      Application.Header.Logo
-    );
-
-    return (
-      <Container className="Application">
-        <Header>
-          <Header.Item>
-            <Header.Menu>
-              <MenuIcon />
-            </Header.Menu>
-            {logo}
-          </Header.Item>
-          <Header.Item>perfil aqui</Header.Item>
-        </Header>
-        <Main>{main}</Main>
-      </Container>
-    );
-  }
-
-  static Header = {
-    Logo: styled('div', {
-      alignItems: 'center',
-      display: 'flex',
-      width: '60px',
-    }),
-  };
-}
-
 const Container = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   height: '100vh',
 });
+
 const Main = styled('main', {});
 
-function Header({
-  children,
-  ...props
-}: React.PropsWithChildren<{}> & React.HTMLAttributes<HTMLElement>) {
-  const Container = styled('header', {
-    alignItems: 'center',
-    backgroundColor: '$gray',
-    display: 'flex',
-    justifyContent: 'space-between',
-  });
+const Header = styled('header', {
+  alignItems: 'center',
+  backgroundColor: '$gray',
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const HeaderItem = styled('div', {
+  alignItems: 'center',
+  display: 'flex',
+});
+
+const HeaderMenu = styled(Button, {
+  height: 24,
+  overflow: 'hidden',
+  position: 'relative',
+  width: 24,
+  span: {
+    position: 'absolute',
+    left: -1000,
+  },
+});
+
+const HeaderLogo = styled('div', {
+  alignItems: 'center',
+  display: 'flex',
+  width: '60px',
+});
+
+export function Application({ children }: PropsWithChildren<{}>) {
+  const service = ioc.get(ApplicationService);
+  const [user, setUser] = useState<IUser | undefined>();
+
+  async function acaoLogin() {
+    await service.login();
+    setUser(service.usuario);
+  }
+
+  if (!user) {
+    acaoLogin();
+    return <div>Em precesso de login!</div>;
+  }
+
+  const [logo, main] = selectChildrenByType(children, Application.Header.Logo);
+
   return (
-    <Container {...props} className="Header">
-      {children}
+    <Container className="Application">
+      <Header className="Header">
+        <HeaderItem className="Item">
+          <HeaderMenu className="Menu" type="button" stylingMode="text">
+            <span>Menu</span>
+            <MenuIcon />
+          </HeaderMenu>
+          {logo}
+        </HeaderItem>
+        <HeaderItem className="Item">
+          <span>perfil aqui</span>
+        </HeaderItem>
+      </Header>
+      <Main>{main}</Main>
     </Container>
   );
 }
 
-Header.Item = function HeaderItem({
-  children,
-  ...props
-}: React.PropsWithChildren<{}> & React.HTMLAttributes<HTMLElement>) {
-  const Container = styled('div', {
-    alignItems: 'center',
-    display: 'flex',
-  });
-  return (
-    <Container {...props} className="Item">
-      {children}
-    </Container>
-  );
-};
+function ApplicationHeader() {}
 
-Header.Menu = function HeaderMenu({
-  children,
-  ...props
-}: React.PropsWithChildren<{}> & React.HTMLAttributes<HTMLElement>) {
-  const Container = styled(Button, {
-    height: 24,
-    overflow: 'hidden',
-    position: 'relative',
-    width: 24,
-    span: {
-      position: 'absolute',
-      left: -1000,
-    },
-  });
-  return (
-    <Container {...props} className="Menu" type="button" stylingMode="text">
-      <span>Menu</span>
-      {children}
-    </Container>
-  );
-};
+Application.Header = ApplicationHeader;
+ApplicationHeader.Logo = HeaderLogo;
